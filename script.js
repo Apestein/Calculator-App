6221f771fec93a53517fb8b48e98666ce197f90a
annony <ltn2057@protonmail.com>
2022-07-26 16:15:48 -0400

    Added all remaining functionality
    fully completed logic, only need to add html/css for completion

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ btnDivide = document.querySelector('#divide')
 btnEqual = document.querySelector('#equal')
 btnClear = document.querySelector('#clear')
 btnDelete = document.querySelector('#delete')
-display = document.querySelector('#display')
+expression = document.querySelector('#expression')
+result = document.querySelector('#result')
 //
 btn1.onclick = update
 btn2.onclick = update
@@ -30,7 +31,7 @@ btn7.onclick = update
 btn8.onclick = update
 btn9.onclick = update
 btn0.onclick = update
-btnDecimal.onclick = (e) => calculateArray.push(e.target.textContent)
+btnDecimal.onclick = update
 btnAdd.onclick = evaluate
 btnMinus.onclick = evaluate
 btnMultiple.onclick = evaluate
@@ -38,46 +39,55 @@ btnDivide.onclick = evaluate
 btnEqual.onclick = evaluate
 btnClear.onclick = () => {
     calculateArray = []
-    //display.textContent = displayStr
+    expression.textContent = '--'
+    result.textContent = '0'
 }
 btnDelete.onclick = () => {
     calculateArray.pop()
-    //display.textContent = displayStr
+    expression.textContent = calculateArray.join('')
+    if (!calculateArray[0]) expression.textContent = '--'
 }
 //
 function update(e) {
     calculateArray.push(e.target.textContent)
-    display.textContent = calculateArray.join('')
+    expression.textContent = calculateArray.join('')
 }
 
 function error() {
     calculateArray = []
-    display.textContent = 'ERROR!'
+    expression.textContent = 'ERROR!'
+}
+
+function notNumber(num) {
+    if (isNaN(num) && num != '.') return true
 }
 
 function evaluate(e) {
-    if (calculateArray.length >= 3) {
-        let temp = operate(+calculateArray[0], calculateArray[1], +calculateArray[2])
-        calculateArray = []
+    let btnValue = e.target.textContent
+    calculateArray.push(btnValue)
+    let indexOfOp = calculateArray.findIndex(notNumber)
+    let n1 = calculateArray.slice(0, indexOfOp).join('')
+    let operator = calculateArray[indexOfOp]
+    let n2 = calculateArray.slice(indexOfOp+1, calculateArray.length-1).join('')
+    let temp
+    if (n1 && n2) {
+        temp = operate(+n1, operator, +n2)
         if (temp) {
+            calculateArray = []
             calculateArray.push(temp)
-            if (e.target.textContent != '=') calculateArray.push(e.target.textContent)
-            display.textContent = `${calculateArray.join('')}\u00A0\u00A0\u00A0\u00A0result:${temp}`
-        } 
-    } else {
-        if (e.target.textContent === '=') error()
-        else {
-            calculateArray.push(e.target.textContent)
-            display.textContent = calculateArray.join('')
+            if (btnValue != '=') calculateArray.push(btnValue)
+            result.textContent = temp
+            expression.textContent = calculateArray.join('')
         }
-    }
+    } else expression.textContent = calculateArray.join('')
 }
 
 function operate(n1, operator, n2) {
-    if (isNaN(n1) || isNaN(n2)) error()
-    if (operator === '+') return n1+n2
-    else if (operator === '-') return n1-n2
-    else if (operator === '*') return n1*n2
-    else if (operator === '/') return n1/n2
-    else error()
+    if (isNaN(n1) || isNaN(n2) || n2 === 0) error()
+    else {
+        if (operator === '+') return n1+n2
+        else if (operator === '-') return n1-n2
+        else if (operator === '*') return n1*n2
+        else if (operator === '/') return n1/n2
+    }
 }
